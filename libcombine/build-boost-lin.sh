baa5d1536388db679a45b88a708efd8448c1bf52
0u812 <0u812@users.noreply.github.com>
2016-11-03 13:00:29 -0700

    use fPIC for compiling boost

--- libcombine/build-boost-lin.sh
+++ libcombine/build-boost-lin.sh
@@ -7,4 +7,4 @@ export LD_LIBRARY_PATH=/home/user/exc/install/gcc-5.4.0/lib:$LD_LIBRARY_PATH
 
 cd ~/exc/src/boost_1_62_0
 ./bootstrap.sh --with-toolset=gcc --prefix=/home/user/exc/install/boost-1.62
-./b2 cxxflags=-std=c++17 -j4 install
+./b2 cxxflags="-std=c++17 -fPIC" -j4 install
