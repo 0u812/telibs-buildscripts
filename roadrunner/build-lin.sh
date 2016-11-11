47e1f48ac796c6d700b7c7513b7e6dc264160e77
0u812 <0u8120u812@users.noreply.github.com>
2016-11-11 17:26:17 +0000

    install numpy and fix python bin dir

--- roadrunner/build-lin.sh
+++ roadrunner/build-lin.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-PYTHON_DIR=/opt/python/cp27-cp27m/bin
+PYTHON_DIR=/opt/python/cp27-cp27m
 PYTHON_EXE=python2
 PYTHON_NAME=python2.7
+PIP=/opt/python/cp27-cp27m/bin/pip2
 
 # cd to directory of this script
 cd "$( dirname "${BASH_SOURCE[0]}" )"
@@ -16,8 +17,10 @@ cd ../..
 ROOT=`pwd`
 echo "Using root dir $ROOT"
 
+$PIP install numpy
+
 cd build
 mkdir -p roadrunner
 cd $_
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/roadrunner -DTHIRD_PARTY_INSTALL_FOLDER=$ROOT/install/roadrunner -DLLVM_CONFIG_EXECUTABLE=$ROOT/install/llvm-3.3/bin/llvm-config -DBUILD_PYTHON=ON -DBUILD_TESTS=ON -DBUILD_TEST_TOOLS=ON -DPYTHON_EXECUTABLE=$PYTHON_DIR/$PYTHON_EXE -DPYTHON_INCLUDE_DIR=$PYTHON_DIR/include/$PYTHON_NAME -DBUILD_JAVA_INTERFACE=OFF -DRR_USE_CXX11=FALSE -DUSE_TR1_CXX_NS=TRUE $ROOT/src/roadrunner
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/roadrunner -DTHIRD_PARTY_INSTALL_FOLDER=$ROOT/install/roadrunner -DLLVM_CONFIG_EXECUTABLE=$ROOT/install/llvm-3.3/bin/llvm-config -DBUILD_PYTHON=ON -DBUILD_TESTS=ON -DBUILD_TEST_TOOLS=ON -DPYTHON_EXECUTABLE=$PYTHON_DIR/bin/$PYTHON_EXE -DPYTHON_INCLUDE_DIR=$PYTHON_DIR/include/$PYTHON_NAME -DBUILD_JAVA_INTERFACE=OFF -DRR_USE_CXX11=FALSE -DUSE_TR1_CXX_NS=TRUE $ROOT/src/roadrunner
 make -j4 install
