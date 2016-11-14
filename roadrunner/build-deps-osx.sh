0997a7d0e584b259cf7bd3c0e4e71c00f720600a
0u812 <0u812@users.noreply.github.com>
2016-11-14 13:04:56 -0800

    add 10.9 deployment target for rr deps

--- roadrunner/build-deps-osx.sh
+++ roadrunner/build-deps-osx.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p ~/devel/build/libroadrunner-deps-xcode
 cd $_
 pwd
-cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/devel/install/libroadrunner-deps-xcode ~/devel/src/libroadrunner-deps
+cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX=~/devel/install/libroadrunner-deps-xcode ~/devel/src/libroadrunner-deps
 xcodebuild -configuration Release build install -target install
 
 # copy to roadrunner-xcode-py2 and roadrunner-xcode-py3
