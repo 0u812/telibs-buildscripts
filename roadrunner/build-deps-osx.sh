a8262c4f40ffedb48087bd2cd4ca8e065c935795
0u812 <0u812@users.noreply.github.com>
2016-11-14 13:02:36 -0800

    add files for building roadrunner on os x

---
+++ roadrunner/build-deps-osx.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p ~/devel/build/libroadrunner-deps-xcode
+cd $_
+pwd
+cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/devel/install/libroadrunner-deps-xcode ~/devel/src/libroadrunner-deps
+xcodebuild -configuration Release build install -target install
+
+# copy to roadrunner-xcode-py2 and roadrunner-xcode-py3
+rsync -av ~/devel/install/libroadrunner-deps-xcode/ ~/devel/install/roadrunner-xcode-py2
+rsync -av ~/devel/install/libroadrunner-deps-xcode/ ~/devel/install/roadrunner-xcode-py3
---
+++ roadrunner/build-py2-osx.sh
@@ -0,0 +1,5 @@
+#/usr/bin/env zsh
+set -e
+
+cd ~/devel/build/roadrunner-xcode
+cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_CXX_FLAGS="-I/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/numpy/core/include" -DCMAKE_INSTALL_PREFIX=~/devel/install/roadrunner-xcode -DLLVM_CONFIG_EXECUTABLE=/Users/phantom/etc/install/llvm-3.5.2-xcode/bin/llvm-config -DTHIRD_PARTY_INSTALL_FOLDER=~/devel/install/roadrunner-xcode -DBUILD_PYTHON=ON -DBUILD_TESTS=ON -DBUILD_TEST_TOOLS=ON -DRR_USE_CXX11=False -DUSE_TR1_CXX_NS=OFF -DPYTHON_EXECUTABLE=/usr/local/bin/python2 -DPYTHON_LIBRARY=/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib -DPYTHON_INCLUDE_DIR=/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 -DWITH_CONDA_BUILDER=OFF -DZLIB_LIBRARY_RELEASE=/usr/lib/libz.1.2.5.dylib -DZLIB_LIBRARY=/usr/lib/libz.1.2.5.dylib ~/devel/src/roadrunner
