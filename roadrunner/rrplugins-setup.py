eb6b1d08437e7f3e4cdcdbb3b908b5cf7cd5626c
0u812 <0u812@users.noreply.github.com>
2016-11-16 10:17:00 -0800

    bump rrplugins ver and required rr ver

--- roadrunner/rrplugins-setup.py
+++ roadrunner/rrplugins-setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(name='rrplugins',
     author='J Kyle Medley, M T Karlsson, Herbert M Sauro',
     author_email='tellurium-discuss@u.washington.edu',
-    version='1.1.3',
+    version='1.1.4',
     description='Plugins for libroadrunner',
     url='http://libroadrunner.org',
     packages=['rrplugins'],
     package_dir={
         'rrplugins' : 'site-packages/rrplugins',
     },
-    install_requires=['libroadrunner>=1.4.9', 'matplotlib>=1.5'],
+    install_requires=['libroadrunner>=1.4.10', 'matplotlib>=1.5'],
 )
