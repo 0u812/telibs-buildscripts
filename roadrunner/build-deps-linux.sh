171d32e55f4cbad39a8ad2b34aa0f82d72fd3c38
0u812 <0u812@users.noreply.github.com>
2016-11-16 14:14:57 -0800

    fix xcodebuild -> make

--- roadrunner/build-deps-linux.sh
+++ roadrunner/build-deps-linux.sh
@@ -11,9 +11,9 @@ mkdir -p $ROOT/build/libroadrunner-deps
 cd $_
 pwd
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX=$ROOT/install/libroadrunner-deps $ROOT/src/libroadrunner-deps
-xcodebuild -configuration Release build install -target install
+make -j4 && make install
 
 # copy to roadrunner-xcode-py2 and roadrunner-xcode-py3
 rsync -av $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py2m
 rsync -av $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py2mu
-rsync -av $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py3
+rsync -av $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py3m
