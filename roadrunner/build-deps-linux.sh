915f6112b63d3babbfc73af674fe937285287528
0u812 <0u812@users.noreply.github.com>
2016-11-16 14:13:06 -0800

    add build script for py2m manylinux

---
+++ roadrunner/build-deps-linux.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+ROOT=/whlbldr
+
+mkdir -p $ROOT/build/libroadrunner-deps
+cd $_
+pwd
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX=$ROOT/install/libroadrunner-deps $ROOT/src/libroadrunner-deps
+xcodebuild -configuration Release build install -target install
+
+# copy to roadrunner-xcode-py2 and roadrunner-xcode-py3
+rsync -av $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py2m
+rsync -av $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py2mu
+rsync -av $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py3
