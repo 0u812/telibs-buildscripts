3d613c955bd3a7f40f766c53649ebfadb6373a4b
0u812 <0u812@users.noreply.github.com>
2016-11-15 17:15:53 -0800

    fix name of roadrunner package

--- roadrunner/rrplugins-setup.py
+++ roadrunner/rrplugins-setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(name='rrplugins',
     author='J Kyle Medley, M T Karlsson, Herbert M Sauro',
     author_email='tellurium-discuss@u.washington.edu',
-    version='1.1.1',
+    version='1.1.2',
     description='Plugins for libroadrunner',
     url='http://libroadrunner.org',
     packages=['rrplugins'],
     package_dir={
         'rrplugins' : 'site-packages/rrplugins',
     },
-    install_requires=['roadrunner>=1.4.9'],
+    install_requires=['libroadrunner>=1.4.9'],
 )
