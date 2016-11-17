640a1c94337c9ced04fb6d6616315a37eb650276
0u812 <0u812@github.com>
2016-11-17 15:55:03 -0800

    get msbuild syntax right

--- roadrunner/build-deps-win64-vs14.sh
+++ roadrunner/build-deps-win64-vs14.sh
@@ -8,14 +8,16 @@ set -o verbose
 ROOT=/c/Users/phantom/Documents/devel
 CMAKE=/c/Users/phantom/Downloads/cmake-3.7.0-win64-x64/bin/cmake
 GIT=git
-MSBUILD=/c/Program\ Files\ \(x86\)/MSBuild/14.0/Bin/MSBuild.exe
+MSBUILD="/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe"
+
+MC="C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Bin/MC.Exe"
 
 mkdir -p $ROOT/build/libroadrunner-deps-vs14-64-2
 cd $_
 pwd
-cmake -G"Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/libroadrunner-deps-vs14-64 $ROOT/src/libroadrunner-deps
+"$CMAKE" -G"Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/libroadrunner-deps-vs14-64 -DCMAKE_MC_COMPILER=$MC $ROOT/src/libroadrunner-deps
 # http://stackoverflow.com/questions/1629779/msbuild-builds-defaults-to-debug-configuration
-$MSBUILD libroadrunner_deps.sln /p:Configuration=Release /t INSTALL
+"$MSBUILD" libroadrunner_deps.sln /property:Configuration=Release /target:install
 
 # copy to roadrunner-xcode-py2 and roadrunner-xcode-py3
 #cp -R $ROOT/install/libroadrunner-deps/ $ROOT/install/roadrunner-py2m
