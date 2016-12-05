8bcab0a8282c935e7d2c24194eea677e48e3e36d
0u812 <0u812@users.noreply.github.com>
2016-12-05 10:27:38 -0800

    enable fbc

--- antimony/build-libsbml-osx.sh
+++ antimony/build-libsbml-osx.sh
@@ -8,5 +8,5 @@ set -o verbose
 mkdir -p ~/devel/build/libsbml-experimental-xcode
 cd $_
 pwd
-cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX=~/devel/install/libsbml-experimental-xcode -DENABLE_DISTRIB=ON -DENABLE_COMP=ON -DENABLE_LAYOUT=ON -DENABLE_RENDER=ON -DWITH_PYTHON=OFF /Users/phantom/devel/src/libsbml-experimental
+cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX=~/devel/install/libsbml-experimental-xcode -DENABLE_DISTRIB=ON -DENABLE_COMP=ON -DENABLE_LAYOUT=ON -DENABLE_RENDER=ON -DENABLE_FBC=ON -DWITH_PYTHON=OFF /Users/phantom/devel/src/libsbml-experimental
 xcodebuild -configuration Release build install -target install
