ac1bb59a17f0c4eb38d182599934bdbae8dbcef9
0u812 <0u812@users.noreply.github.com>
2016-12-06 11:36:35 -0800

    more specific instructions for uploading and signing

--- antimony/build-py2m-osx.sh
+++ antimony/build-py2m-osx.sh
@@ -17,8 +17,9 @@ cd $_
 pwd
 cmake -GXcode -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ANTIMONY_INSTALL -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DWITH_SBML=ON -DWITH_STATIC_SBML=ON -DWITH_LIBSBML_LIBXML=ON -DWITH_PYTHON=ON -DLIBSBML_INSTALL_DIR=$LIBSBML -DWITH_LIBSBML_COMPRESSION=ON -DPYTHON_EXECUTABLE=$PYTHON -DPYTHON_LIBRARY=$PYLIB -DPYTHON_INCLUDE_DIR=$PYINCLUDE -DWITH_QTANTIMONY=OFF -DPYTHON_LOCAL_INSTALL=ON -DPYTHON_SYSTEM_INSTALL=OFF -DWITH_CONDA_BUILDER=OFF -DWITH_CELLML=ON -DCELLML_API_INSTALL_DIR=$CELLAPI  ~/devel/src/antimony-trunk
 xcodebuild -configuration Release build install -target install
+cmake -GXcode -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ANTIMONY_INSTALL -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DWITH_SBML=ON -DWITH_STATIC_SBML=ON -DWITH_LIBSBML_LIBXML=ON -DWITH_PYTHON=ON -DLIBSBML_INSTALL_DIR=$LIBSBML -DWITH_LIBSBML_COMPRESSION=ON -DPYTHON_EXECUTABLE=$PYTHON -DPYTHON_LIBRARY=$PYLIB -DPYTHON_INCLUDE_DIR=$PYINCLUDE -DWITH_QTANTIMONY=OFF -DPYTHON_LOCAL_INSTALL=ON -DPYTHON_SYSTEM_INSTALL=OFF -DWITH_CONDA_BUILDER=OFF -DWITH_CELLML=ON -DCELLML_API_INSTALL_DIR=$CELLAPI  ~/devel/src/antimony-trunk
 xcodebuild -configuration Release build install -target install
-#xcodebuild -configuration Release build install -target install
+xcodebuild -configuration Release build install -target install
 
 # copy over cellml libs
 cp $CELLAPI/lib/libcellml.1.13.dylib $ANTIMONY_INSTALL/bindings/python/antimony/libcellml.2.dylib
@@ -66,4 +67,4 @@ cd dist
 find . -name 'antimony*none*' -type f -exec bash -c 'mv "$1" "${1/none/cp27m}" ' -- \{\} \;
 cd ..
 
-echo "Now do something like /Library/Frameworks/Python.framework/Versions/2.7/bin/twine upload --sign 9BE0E97B $ANTIMONY_INSTALL/bindings/python/dist/..."
+echo "Now do something like /Library/Frameworks/Python.framework/Versions/2.7/bin/twine upload -s --sign-with gpg2 -i 9BE0E97B /Users/phantom/devel/install/antimony-trunk-xcode-py2m/bindings/python/dist/ ..."
