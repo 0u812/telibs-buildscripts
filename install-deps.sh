c8e5c581bf364b447f64c8709dae66bb8b4ce0ea
0u812 <0u812@users.noreply.github.com>
2016-12-09 18:26:14 +0000

    fix paths in cell build script

---
+++ install-deps.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+yum -y install libxml2-devel gpg openssl-devel expat-devel gettext-devel zlib-devel bzip2-devel pcre-develi
+
+# add gcc paths
+#export LD_LIBRARY_PATH=/whlbldr/install/gcc-5.4.0/lib:$LD_LIBRARY_PATH
+#export PATH=/whlbldr/install/gcc-5.4.0/bin:$PATH
+
+rsync -av /whlbldr/.ssh ~
+
+git config --global user.email 0u812@users.noreply.github.com
+git config --global user.name  0u812
