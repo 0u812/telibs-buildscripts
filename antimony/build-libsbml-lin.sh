f6bc356626e5dc379d3756b27e7e77198b6c720a
0u812 <0u812@users.noreply.github.com>
2016-12-09 20:54:46 +0000

    add libsbml build script for lin

---
+++ antimony/build-libsbml-lin.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+ROOT=/whlbldr
+
+mkdir -p $ROOT/build/libsbml-experimental
+cd $_
+pwd
+cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/libsbml-experimental -DENABLE_DISTRIB=ON -DENABLE_COMP=ON -DENABLE_LAYOUT=ON -DENABLE_RENDER=ON -DENABLE_FBC=ON -DWITH_PYTHON=OFF $ROOT/src/libsbml-experimental
+make -j4 && make install
