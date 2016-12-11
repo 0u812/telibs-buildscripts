9e39761a0c2ecae447ae67a24f777aeb97d14b4d
0u812 <0u812@github.com>
2016-12-11 12:07:15 -0800

    specify bison/flex dirs

--- antimony/build-cellapi-win64.sh
+++ antimony/build-cellapi-win64.sh
@@ -12,6 +12,7 @@ GIT=git
 DEVENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
 BISON="C:/Users/phantom/Downloads/win_flex_bison-2.5.6/win_bison.exe"
 FLEX="C:/Users/phantom/Downloads/win_flex_bison-2.5.6/win_flex.exe"
+FLEXINCL="C:/Users/phantom/Downloads/win_flex_bison-2.5.6"
 
 OMNIIDL="C:\Users\phantom\Documents\exc\src\omniORB-4.2.1-2\omniORB-4.2.1\bin\x86_win32\omniidl.exe"
 
@@ -23,5 +24,5 @@ mkdir -p $ROOT/build/cellapi
 cd $_
 rm -rf *
 pwd
-cmake -G"Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/cell-api -DOMNIIDL=$OMNIIDL -DENABLE_CUSES=TRUE -DENABLE_ANNOTOOLS=TRUE -DENABLE_CEVAS=TRUE -DENABLE_TELICEMS=TRUE -DENABLE_CCGS=ON -DENABLE_CELEDS=ON -DENABLE_CELEDS_EXPORTER=ON -DENABLE_MALAES=ON -DENABLE_RDF=ON -DENABLE_VACSS=ON -DCMAKE_CXX_FLAGS="/IC:\Users\phantom\Downloads\win_flex_bison-2.5.6" $ROOT/src/cellml-api
+cmake -G"Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/cell-api -DOMNIIDL=$OMNIIDL -DENABLE_CUSES=TRUE -DENABLE_ANNOTOOLS=TRUE -DENABLE_CEVAS=TRUE -DENABLE_TELICEMS=TRUE -DENABLE_CCGS=ON -DENABLE_CELEDS=ON -DENABLE_CELEDS_EXPORTER=ON -DENABLE_MALAES=ON -DENABLE_RDF=ON -DENABLE_VACSS=ON -DCMAKE_CXX_FLAGS="/IC:\Users\phantom\Downloads\win_flex_bison-2.5.6" -DBISON_EXECUTABLE=$BISON -DFLEX_EXECUTABLE=$FLEX -DFLEX_INCLUDE_DIR=$FLEXINCL $ROOT/src/cellml-api
 cmake --build . --target install --config Release
