cbfc0d684d4fedc24b5daa85e5a07157242107c9
0u812 <0u812@users.noreply.github.com>
2016-12-21 13:10:28 -0800

    build bin whl

--- phrasedml/build-py2m-osx.sh
+++ phrasedml/build-py2m-osx.sh
@@ -34,3 +34,13 @@ pwd
 cmake -GXcode -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DLIBSEDML_LIBRARY=$LIBSEDML -DLIBSEDML_INCLUDE_DIR=$LIBSEDML_INCLUDE -DLIBSBML_LIBRARY=$LIBSBML -DLIBSBML_INCLUDE_DIR=$LIBSBML_INCLUDE -DLIBNUML_LIBRARY=$LIBNUML -DLIBNUML_INCLUDE_DIR=$LIBNUML_INCLUDE -DEXTRA_LIBS='xml2;bz2;z;iconv' -DWITH_PYTHON=ON -DPYTHON_LOCAL_INSTALL=ON -DPYTHON_SYSTEM_INSTALL=OFF -DWITH_CONDA_BUILDER=OFF -DPYTHON_EXECUTABLE=$PYTHON -DPYTHON_LIBRARY=$PYLIB -DPYTHON_INCLUDE_DIR=$PYINCLUDE ~/devel/src/phrasedml
 
 xcodebuild -target install -configuration Release
+
+cd $INSTALL_DIR/bindings/python
+python2 setup.py bdist_wheel --python-tag=cp27 --plat-name=macosx-10.9-x86_64
+
+# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
+cd dist
+find . -name 'phrasedml*none*' -type f -exec bash -c 'mv "$1" "${1/none/cp27m}" ' -- \{\} \;
+cd ..
+
+echo "Now do something like /Library/Frameworks/Python.framework/Versions/2.7/bin/twine upload -s --sign-with gpg2 -i 9BE0E97B $INSTALL_DIR/bindings/python/dist/ ..."
