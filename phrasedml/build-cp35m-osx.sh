0bfb6212e8c1cd47cfc32619917bf6c6455316ae
0u812 <0u812@users.noreply.github.com>
2016-12-22 10:05:20 -0800

    build twice

--- phrasedml/build-cp35m-osx.sh
+++ phrasedml/build-cp35m-osx.sh
@@ -8,6 +8,7 @@ set -o verbose
 # cd to the directory of this script
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 THIS_DIR=`pwd`
+NAME=phrasedml-cp35m-xcode
 
 PYTHON=/usr/local/bin/python3
 PYLIB=/Library/Frameworks/Python.framework/Versions/3.5/lib/libpython3.5m.dylib
@@ -26,11 +27,12 @@ LIBSEDML=$LIBSEDML_PREFIX/lib/libsedml-static.a
 LIBSEDML_INCLUDE=$LIBSEDML_PREFIX/include
 
 INSTALL_PREFIX=/Users/phantom/devel/install
-INSTALL_DIR=$INSTALL_PREFIX/phrasedml-xcode-py3m
+INSTALL_DIR=$INSTALL_PREFIX/$NAME
 
-mkdir -p ~/devel/build/phrasedml-xcode-py3m
+mkdir -p ~/devel/build/$NAME
 cd $_
 pwd
+#rm -rf *
 cmake -GXcode -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DLIBSEDML_LIBRARY=$LIBSEDML -DLIBSEDML_INCLUDE_DIR=$LIBSEDML_INCLUDE -DLIBSBML_LIBRARY=$LIBSBML -DLIBSBML_INCLUDE_DIR=$LIBSBML_INCLUDE -DLIBNUML_LIBRARY=$LIBNUML -DLIBNUML_INCLUDE_DIR=$LIBNUML_INCLUDE -DEXTRA_LIBS='xml2;bz2;z;iconv' -DWITH_PYTHON=ON -DPYTHON_LOCAL_INSTALL=ON -DPYTHON_SYSTEM_INSTALL=OFF -DWITH_CONDA_BUILDER=OFF -DPYTHON_EXECUTABLE=$PYTHON -DPYTHON_LIBRARY=$PYLIB -DPYTHON_INCLUDE_DIR=$PYINCLUDE ~/devel/src/phrasedml
 
 xcodebuild -target install -configuration Release
