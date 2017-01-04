d4d248836f201a791ed5c73aeb2186270bddb5f6
0u812 <0u812@users.noreply.github.com>
2017-01-04 11:05:23 -0800

    add osx builder for libnuml

--- libcombine/zipper-osx-10.9.sh
+++ libcombine/zipper-osx-10.9.sh
@@ -9,11 +9,12 @@ set -o verbose
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 THIS_DIR=`pwd`
 SRCNAME=zipper
-INSTNAME=$SRCNAME-osx-10.9
+OSX_VER=10.9
+INSTNAME=$SRCNAME-osx-$OSX_VER
 ROOT=~/devel
 export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
 
-mkdir -p ~/devel/build/libsedml-xcode
+mkdir -p $ROOT/build/$INSTNAME
 cd $_
 rm -rf *
 cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_INSTALL_PREFIX=$ROOT/install/$INSTNAME  $ROOT/src/$SRCNAME
--- libnuml/libnuml-osx-10.9.sh
+++ libnuml/libnuml-osx-10.9.sh
@@ -8,23 +8,21 @@ set -o verbose
 # cd to the directory of this script
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 THIS_DIR=`pwd`
-SRCNAME=libsedml
+SRCNAME=libnuml
 OSX_VER=10.9
 INSTNAME=$SRCNAME-osx-$OSX_VER
-ROOT=/Users/phantom/devel
+ROOT=~/devel
 export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX$OSX_VER.sdk
 
 LIBSBML_INSTNAME=libsbml-experimental-osx-$OSX_VER
 LIBSBML_INSTALL_DIR=$ROOT/install/$LIBSBML_INSTNAME
 LIBSBML=$LIBSBML_INSTALL_DIR/lib/libsbml-static.a
 LIBSBML_INCLUDE=$LIBSBML_INSTALL_DIR/include
-NUML=$ROOT/install/libnuml-xcode/lib/libnuml-static.a
-NUML_INCLUDE=$ROOT/install/libnuml-xcode/include
 
 export CMAKE_PREFIX_PATH="$LIBSBML_INSTALL_DIR/lib/cmake"
 
 mkdir -p $ROOT/build/$INSTNAME
 cd $_
 rm -rf *
-cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VER -DCMAKE_INSTALL_PREFIX=$ROOT/install/$INSTNAME -DLIBSBML_LIBRARY=$LIBSBML -DLIBSBML_INCLUDE_DIR=$LIBSBML_INCLUDE -DLIBSBML_STATIC=ON -DEXTRA_LIBS='xml2;bz2;z;iconv' -DWITH_PYTHON=OFF-DLIBNUML_LIBRARY=$NUML -DLIBNUML_INCLUDE_DIR=$NUML_INCLUDE $ROOT/src/$SRCNAME
+cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VER -DCMAKE_INSTALL_PREFIX=$ROOT/install/$INSTNAME -DLIBSBML_LIBRARY=$LIBSBML -DLIBSBML_INCLUDE_DIR=$LIBSBML_INCLUDE -DLIBSBML_STATIC=ON -DEXTRA_LIBS='xml2;bz2;z;iconv' $ROOT/src/NuML/$SRCNAME
 xcodebuild -configuration Release build install -target install
