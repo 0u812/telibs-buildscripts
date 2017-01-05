5ac3eef4052f83adcd0f15cbab6948c0a48cdc5a
0u812 <0u812@users.noreply.github.com>
2017-01-05 12:33:38 -0800

    separate cmake / os-specific builds for libsbml

--- libsbml/libsbml-experimental-osx-10.9.sh
+++ libsbml/libsbml-experimental-osx-10.9.sh
@@ -14,8 +14,8 @@ INSTNAME=$SRCNAME-osx-$OSX_VER
 ROOT=~/devel
 export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX$OSX_VER.sdk
 
-mkdir -p $ROOT/build/$INSTNAME
-cd $_
-rm -rf *
-cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VER -DCMAKE_INSTALL_PREFIX=$ROOT/install/$INSTNAME -DENABLE_DISTRIB=ON -DENABLE_COMP=ON -DENABLE_LAYOUT=ON -DENABLE_RENDER=ON -DENABLE_FBC=ON -DWITH_PYTHON=OFF $ROOT/src/$SRCNAME
-xcodebuild -configuration Release build install -target install
+CMAKE_GEN="-GXcode"
+CMAKE_PLATFORM_FLAGS=( "-DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VER" )
+CMAKE_BUILD_CMD="xcodebuild -configuration Release build install -target install"
+
+source libsbml-experimental-build.sh
