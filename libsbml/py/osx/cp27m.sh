a8fb05c598f9f05b7fdb756b7540ae4fa9aac4a8
0u812 <0u812@users.noreply.github.com>
2017-01-05 15:06:41 -0800

    add cp27 for osx builder

---
+++ libsbml/py/osx/cp27m.sh
@@ -0,0 +1,32 @@
+#/usr/bin/env zsh
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+# cd to the directory of this script
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+THIS_DIR=`pwd`
+ROOT=~/devel
+
+export CPVER=cp27
+export CP=cp27m
+PYTHON_DIR=/Users/phantom/.pyenv/versions/2.7.13
+export PYTHON=$PYTHON_DIR/bin/python
+export PYTHON_INCLUDE=$PYTHON_DIR/include/python2.7
+export WHEEL_DIR=$ROOT/src/wheels/libsbml-experimental
+mkdir -p $WHEEL_DIR
+
+# build the project
+source $THIS_DIR/../../libsbml-experimental-osx-10.9.sh
+exit
+# copy over setup.py
+cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python2.7/site-packages
+$PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=macosx-10.9-x86_64
+
+# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
+cd dist
+find . -name 'libsbml_experimental*none*' -type f -exec bash -c 'mv "$1" "${1/none/${CP}}" ' -- \{\} \;
+cd ..
+cp dist/* $WHEEL_DIR
