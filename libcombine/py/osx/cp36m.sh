aa71c00a894325178162bc04a442fd1e405b5f89
0u812 <0u812@users.noreply.github.com>
2017-01-04 20:18:09 -0800

    add libcombine osx cp36m builder

---
+++ libcombine/py/osx/cp36m.sh
@@ -0,0 +1,30 @@
+#/usr/bin/env zsh
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+# cd to the directory of this script
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+THIS_DIR=`pwd`
+
+export CP=cp36m
+PYTHON_DIR=/Users/phantom/.pyenv/versions/3.6.0
+export PYTHON=$PYTHON_DIR/bin/python
+export PYTHON_INCLUDE=$PYTHON_DIR/include/python3.6m
+
+# build the project
+source $THIS_DIR/../../libcombine-osx-10.9.sh
+# copy over setup.py
+cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python3.6/site-packages
+# rename libcombine.py to __init__.py per Frank
+mv $ROOT/install/$INSTNAME/lib/python3.6/site-packages/libcombine/libcombine.py $ROOT/install/$INSTNAME/lib/python3.6/site-packages/libcombine/__init__.py
+cd $ROOT/install/$INSTNAME/lib/python3.6/site-packages
+$PYTHON setup.py bdist_wheel --python-tag=$CP --plat-name=macosx-10.9-x86_64
+
+# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
+cd dist
+find . -name 'libcombine*none*' -type f -exec bash -c 'mv "$1" "${1/none/${CP}}" ' -- \{\} \;
+cd ..
+cp dist/* ~/wheelhouse
