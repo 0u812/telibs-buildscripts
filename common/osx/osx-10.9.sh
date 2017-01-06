43fccf95b907793c4ab5e8e029d746a921928f0d
0u812 <0u812@users.noreply.github.com>
2017-01-06 11:39:32 -0800

    separate out common platform defs

---
+++ common/osx/osx-10.9.sh
@@ -0,0 +1,21 @@
+#/usr/bin/env zsh
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+OSX_VER=10.9
+OS_STR=osx-$OSX_VER
+ROOT=~/devel
+export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX$OSX_VER.sdk
+
+CMAKE_GEN="-GXcode"
+CMAKE_PLATFORM_FLAGS=( "-DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VER" )
+CMAKE_BUILD_CMD="xcodebuild -configuration Release build install -target install"
+
+LIBSBML_INSTNAME=libsbml-experimental-$OS_STR
+LIBSBML_INSTALL_DIR=$ROOT/install/$LIBSBML_INSTNAME
+LIBSBML=$LIBSBML_INSTALL_DIR/lib/libsbml-static.a
+LIBSBML_INCLUDE=$LIBSBML_INSTALL_DIR/include
+LIBSBML_EXTRA_LIBS='xml2;bz2;z;iconv'
--- libnuml/build.sh
+++ libnuml/build.sh
@@ -8,22 +8,18 @@ set -o verbose
 # cd to the directory of this script
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 THIS_DIR=`pwd`
+
 SRCNAME=libnuml
-OSX_VER=10.9
-OS_STR=osx-$OSX_VER
 INSTNAME=$SRCNAME-$OS_STR
-ROOT=~/devel
-export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX$OSX_VER.sdk
 
 LIBSBML_INSTNAME=libsbml-experimental-$OS_STR
 LIBSBML_INSTALL_DIR=$ROOT/install/$LIBSBML_INSTNAME
 LIBSBML=$LIBSBML_INSTALL_DIR/lib/libsbml-static.a
 LIBSBML_INCLUDE=$LIBSBML_INSTALL_DIR/include
-
 export CMAKE_PREFIX_PATH="$LIBSBML_INSTALL_DIR/lib/cmake"
 
 mkdir -p $ROOT/build/$INSTNAME
 cd $_
 rm -rf *
-cmake -G"Xcode" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VER -DCMAKE_INSTALL_PREFIX=$ROOT/install/$INSTNAME -DLIBSBML_LIBRARY=$LIBSBML -DLIBSBML_INCLUDE_DIR=$LIBSBML_INCLUDE -DLIBSBML_STATIC=ON -DEXTRA_LIBS='xml2;bz2;z;iconv' $ROOT/src/NuML/$SRCNAME
-xcodebuild -configuration Release build install -target install
+cmake $CMAKE_GEN $CMAKE_PLATFORM_FLAGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ROOT/install/$INSTNAME -DLIBSBML_LIBRARY=$LIBSBML -DLIBSBML_INCLUDE_DIR=$LIBSBML_INCLUDE -DLIBSBML_STATIC=ON -DEXTRA_LIBS=$LIBSBML_EXTRA_LIBS $ROOT/src/NuML/$SRCNAME
+eval $CMAKE_BUILD_CMD
