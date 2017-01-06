2b9db3b44ffcdf0045a013c248fd7148ba835c18
0u812 <0u812@users.noreply.github.com>
2017-01-05 17:12:50 -0800

    add setup.py for sbml2matlab

---
+++ sbml2matlab/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='sbml2matlab',
+      version='0.9.0',
+      description='An SBML to MATLAB translator',
+      author='Stanley Gu, Lucian Smith',
+      author_email='stanleygu@gmail.com, lucianoelsmitho@gmail.com',
+      url='https://github.com/stanleygu/sbml2matlab',
+      packages=['sbml2matlab'],
+      package_data={
+          # add dll, won't hurt unix, not there anyway
+          "sbml2matlab": ["_sbml2matlab.pyd", "*.dll", "*.txt",
+                          "*.lib", "*.so"]
+      })
