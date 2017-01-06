3e8530b6ced740616e8680b9c845bad6f87c2809
0u812 <0u812@users.noreply.github.com>
2017-01-06 10:34:22 -0800

    make libsbml python builders uniform

--- libsbml/py/osx/cp35m.sh
+++ libsbml/py/osx/cp35m.sh
@@ -5,24 +5,17 @@ set -e
 # echo commands as they are run
 set -o verbose
 
-# cd to the directory of this script
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-THIS_DIR=`pwd`
-ROOT=~/devel
-
 export CPVER=cp35
 export CP=cp35m
 PYTHON_DIR=/Users/phantom/.pyenv/versions/3.5.2
 export PYTHON=$PYTHON_DIR/bin/python
 export PYTHON_INCLUDE=$PYTHON_DIR/include/python3.5m
-export WHEEL_DIR=$ROOT/src/wheelhouse
-mkdir -p $WHEEL_DIR
 
 # build the project
-source $THIS_DIR/../../libsbml-experimental-osx-10.9.sh
+source "$( dirname "${BASH_SOURCE[0]}" )"/../../osx-10.9.sh
 # copy over setup.py
 cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python3.5/site-packages
-# rename libcombine.py to __init__.py per Frank
+# rename libsbml.py to __init__.py
 mv $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsbml/libsbml.py $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsbml/__init__.py
 cd $ROOT/install/$INSTNAME/lib/python3.5/site-packages
 $PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=macosx-10.9-x86_64
