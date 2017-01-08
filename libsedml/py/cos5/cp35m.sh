82d0d5f8b384d31f64a52c273283edb6a29e8a1b
0u812 <0u812@users.noreply.github.com>
2017-01-08 08:29:39 +0000

    add libsedml cos5 cp35/36

---
+++ libsedml/py/cos5/cp35m.sh
@@ -0,0 +1,33 @@
+#/usr/bin/env zsh
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+export CPVER=cp35
+export CP=cp35m
+PYTHON_DIR=/opt/python/cp35-cp35m
+export PYTHON=$PYTHON_DIR/bin/python
+export PYTHON_INCLUDE=$PYTHON_DIR/include/python3.5m
+
+# build the project
+source "$( dirname "${BASH_SOURCE[0]}" )"/../../cos5.sh
+# copy over setup.py
+cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python3.5/site-packages
+# copy over __init__.py
+cp $THIS_DIR/__init__.py $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsedml/
+
+cd $ROOT/install/$INSTNAME/lib/python3.5/site-packages
+# rename to tesedml
+rm -rf tesedml
+mv libsedml tesedml
+
+# build wheel
+$PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=macosx-10.9-x86_64
+
+# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
+cd dist
+find . -name '*none*' -type f -exec bash -c 'mv "$1" "${1/none/${CP}}" ' -- \{\} \;
+cd ..
+cp dist/* $WHEEL_DIR
