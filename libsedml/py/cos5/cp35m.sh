fe82fbe305253043a359989ec2f55c2f8712c71f
0u812 <0u812@users.noreply.github.com>
2017-01-09 19:38:26 +0000

    manylinux: copy libsedml dep libs (z, bz2, xml) to eliminate dep on external libs

--- libsedml/py/cos5/cp35m.sh
+++ libsedml/py/cos5/cp35m.sh
@@ -18,6 +18,12 @@ cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python3.5/site-packages
 # copy over __init__.py
 cp $THIS_DIR/__init__.py $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsedml/
 
+# fix dep libs
+cp /usr/lib64/libbz2.so.1.0.3 $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsedml/libbz2.so.1
+cp /usr/lib64/libxml2.so.2.6.26 $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsedml/libxml2.so.2
+cp /lib64/libz.so.1.2.3 $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsedml/libz.so.1
+patchelf --set-rpath '$ORIGIN/.' $ROOT/install/$INSTNAME/lib/python3.5/site-packages/libsedml/_libsedml.so
+
 cd $ROOT/install/$INSTNAME/lib/python3.5/site-packages
 # rename to tesedml
 rm -rf tesedml
