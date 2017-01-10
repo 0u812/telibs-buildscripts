ced1050f6674a76f6b8c7c029d3a744e0933e582
0u812 <0u812@users.noreply.github.com>
2017-01-10 22:44:09 +0000

    update libsedml build scripts to use common python script

--- libsedml/py/cos5/cp36m.sh
+++ libsedml/py/cos5/cp36m.sh
@@ -1,39 +1,10 @@
-#/usr/bin/env zsh
-
-# exit on failure
-set -e
-# echo commands as they are run
-set -o verbose
+#/usr/bin/env bash
 
 export CPVER=cp36
 export CP=cp36m
+export PYVER2=3.6
 PYTHON_DIR=/opt/python/cp36-cp36m
 export PYTHON=$PYTHON_DIR/bin/python
-export PYTHON_INCLUDE=$PYTHON_DIR/include/python3.6m
+export PYTHON_INCLUDE=$PYTHON_DIR/include/python${PYVER2}m
 
-# build the project
-source "$( dirname "${BASH_SOURCE[0]}" )"/../../cos5.sh
-# copy over setup.py
-cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python3.6/site-packages
-# copy over __init__.py
-cp $THIS_DIR/__init__.py $ROOT/install/$INSTNAME/lib/python3.6/site-packages/libsedml/
-
-# fix dep libs
-cp /usr/lib64/libbz2.so.1.0.3 $ROOT/install/$INSTNAME/lib/python3.6/site-packages/libsedml/libbz2.so.1
-cp /usr/lib64/libxml2.so.2.6.26 $ROOT/install/$INSTNAME/lib/python3.6/site-packages/libsedml/libxml2.so.2
-cp /lib64/libz.so.1.2.3 $ROOT/install/$INSTNAME/lib/python3.6/site-packages/libsedml/libz.so.1
-patchelf --set-rpath '$ORIGIN/.' $ROOT/install/$INSTNAME/lib/python3.6/site-packages/libsedml/_libsedml.so
-
-cd $ROOT/install/$INSTNAME/lib/python3.6/site-packages
-# rename to tesedml
-rm -rf tesedml
-mv libsedml tesedml
-
-# build wheel
-$PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=$WHEEL_PLATFORM
-
-# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
-cd dist
-find . -name '*none*' -type f -exec bash -c 'mv "$1" "${1/none/${CP}}" ' -- \{\} \;
-cd ..
-cp dist/* $WHEEL_DIR
+source "$( dirname "${BASH_SOURCE[0]}" )"/common.sh
