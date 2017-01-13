b69aa39ba8a640e35aa901312e4864696ad4f2c4
0u812 <0u812@users.noreply.github.com>
2017-01-13 02:00:38 +0000

    add phrasedml cos5 python drivers

---
+++ phrasedml/py/cos5/common.sh
@@ -0,0 +1,25 @@
+#/usr/bin/env bash
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+# build the project
+source "$( dirname "${BASH_SOURCE[0]}" )"/../../cos5.sh
+
+# fix dep libs
+cp /usr/lib64/libbz2.so.1.0.3 $ROOT/install/$INSTNAME/bindings/python/phrasedml/libbz2.so.1
+cp /usr/lib64/libxml2.so.2.6.26 $ROOT/install/$INSTNAME/bindings/python/phrasedml/libxml2.so.2
+cp /lib64/libz.so.1.2.3 $ROOT/install/$INSTNAME/bindings/python/phrasedml/libz.so.1
+patchelf --set-rpath '$ORIGIN/.' $ROOT/install/$INSTNAME/bindings/python/phrasedml/_$SRCNAME.so
+
+cd $ROOT/install/$INSTNAME/bindings/python
+
+$PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=$WHEEL_PLATFORM
+
+# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
+cd dist
+find . -name '*none*' -type f -exec bash -c 'mv "$1" "${1/none/${CP}}" ' -- \{\} \;
+cd ..
+cp dist/* $WHEEL_DIR
