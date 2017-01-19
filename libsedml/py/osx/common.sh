0074a680b0289a9bb1547c85bce775a11c3ed36d
0u812 <0u812@users.noreply.github.com>
2017-01-19 10:33:27 -0800

    add libsedml osx python builders

--- libsedml/py/osx/common.sh
+++ libsedml/py/osx/common.sh
@@ -1,24 +1,22 @@
-#/usr/bin/env zsh
+#/usr/bin/env bash
 
 # exit on failure
 set -e
 # echo commands as they are run
 set -o verbose
 
-export CPVER=cp27
-export CP=cp27m
-PYTHON_DIR=/Users/phantom/.pyenv/versions/2.7.13
-export PYTHON=$PYTHON_DIR/bin/python
-export PYTHON_INCLUDE=$PYTHON_DIR/include/python2.7
-
 # build the project
 source "$( dirname "${BASH_SOURCE[0]}" )"/../../osx-10.9.sh
 # copy over setup.py
-cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python2.7/site-packages
+cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python${PYVER2}/site-packages
 # copy over __init__.py
-cp $THIS_DIR/__init__.py $ROOT/install/$INSTNAME/lib/python2.7/site-packages/libsedml
+cp $THIS_DIR/__init__.py $ROOT/install/$INSTNAME/lib/python${PYVER2}/site-packages/libsedml
 
-cd $ROOT/install/$INSTNAME/lib/python2.7/site-packages
+cd $ROOT/install/$INSTNAME/lib/python${PYVER2}/site-packages
+# rename to tesedml
+rm -rf tesedml
+mv libsedml tesedml
+
 $PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=macosx-10.9-x86_64
 
 # fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
