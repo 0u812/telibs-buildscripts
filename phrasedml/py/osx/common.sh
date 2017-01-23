8aaf651bf07b012a2a7e593026bd93daba3b2852
0u812 <0u812@users.noreply.github.com>
2017-01-23 11:51:26 -0800

    add phrasedml osx python builders

---
+++ phrasedml/py/osx/common.sh
@@ -0,0 +1,19 @@
+#/usr/bin/env bash
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+# build the project
+source "$( dirname "${BASH_SOURCE[0]}" )"/../../osx-10.9.sh
+
+cd $ROOT/install/$INSTNAME/bindings/python
+
+$PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=$WHEEL_PLATFORM
+
+# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
+cd dist
+find . -name '*none*' -type f -exec bash -c 'mv "$1" "${1/none/${CP}}" ' -- \{\} \;
+cd ..
+cp dist/* $WHEEL_DIR
---
+++ phrasedml/py/osx/drivers/cp33m.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+export CPVER=cp33
+export CP=cp33m
+PYVER2=3.3
+PYTHON_DIR=/Users/phantom/.pyenv/versions/3.3.6
+export PYTHON=$PYTHON_DIR/bin/python
+export PYTHON_INCLUDE=$PYTHON_DIR/include/python3.3m
+
+source "$( dirname "${BASH_SOURCE[0]}" )"/../common.sh
