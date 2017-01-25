1fdec0085998c79fbb0322bae04640aacd47c41b
0u812 <0u812@github.com>
2017-01-25 11:43:19 -0800

    add libsedml win python builders

---
+++ libsedml/py/win/drivers/cp35m.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+export CPVER=cp35
+export CP=cp35m
+PYVER2=3.5
+PYTHON_DIR="C:\Pythons\win64-vs14\cp35m"
+export PYTHON="$PYTHON_DIR\python.exe"
+export PYTHON_INCLUDE="$PYTHON_DIR\include"
+export PYTHON_LIB="$PYTHON_DIR\libs\python35.lib"
+export PYTHON_LINKING="-DPYTHON_LIBRARY=$PYTHON_LIB"
+
+source "$( dirname "${BASH_SOURCE[0]}" )"/../common.sh
