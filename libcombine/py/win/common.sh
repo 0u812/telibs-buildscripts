658885693fe205641798afba17decf5260498453
0u812 <0u812@github.com>
2017-01-24 17:19:12 -0800

    fix zipper for windows

---
+++ libcombine/py/win/common.sh
@@ -0,0 +1,26 @@
+#/usr/bin/env bash
+
+# exit on failure
+set -e
+# echo commands as they are run
+set -o verbose
+
+# build the project
+source "$( dirname "${BASH_SOURCE[0]}" )"/../../win64-vs14.sh
+# copy over setup.py
+cp $THIS_DIR/setup.py $ROOT/install/$INSTNAME/lib/python${PYVER2}/site-packages
+# copy over __init__.py
+cp $THIS_DIR/__init__.py $ROOT/install/$INSTNAME/lib/python${PYVER2}/site-packages/$SRCNAME
+
+cd $ROOT/install/$INSTNAME/lib/python${PYVER2}/site-packages
+# rename to tecombine
+rm -rf tecombine
+mv $SRCNAME tecombine
+
+$PYTHON setup.py bdist_wheel --python-tag=$CPVER --plat-name=$WHEEL_PLATFORM
+
+# fix ABI tag http://stackoverflow.com/questions/9393607/find-and-replace-filename-recursively-in-a-directory
+cd dist
+find . -name '*none*' -type f -exec bash -c 'mv "$1" "${1/none/${CP}}" ' -- \{\} \;
+cd ..
+cp dist/* $WHEEL_DIR
---
+++ libcombine/py/win/drivers/cp34m.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env zsh
+
+export CPVER=cp34
+export CP=cp34m
+PYVER2=3.4
+PYTHON_DIR=/Users/phantom/.pyenv/versions/3.4.5
+export PYTHON=$PYTHON_DIR/bin/python
+export PYTHON_INCLUDE=$PYTHON_DIR/include/python3.4m
+
+source "$( dirname "${BASH_SOURCE[0]}" )"/../common.sh
